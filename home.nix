1594994116ffe19607ae60aafea0ffcf03a9e9e2
Hana Volků <volek@adamv.cz>
2025-11-16 21:20:17 +0100

    fastfetch lol
    The repo desperately needs some cleanup. Let's tweak fastfetch instead
    :3

--- home.nix
+++ home.nix
@@ -11,6 +11,51 @@
     enableZshIntegration = true;
   };
 
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      logo = {
+        source = "${./lix-logo.txt}";
+        type = "file";
+        color = {
+          # The original logo is themed after the lesbian flag and looks great
+          # on a light background, but I had to desaturate the colors a bit
+          # for it to look good in terminal as well.
+          "1" = "#d362a4"; #d362a4
+          "2" = "#d54620"; #d52d00
+          "3" = "#a3206e"; #a30262
+          "4" = "#ff9a56"; #ff9a56
+        };
+      };
+      modules = [
+        "title"
+        "separator"
+        "os"
+        "host"
+        "kernel"
+        "uptime"
+        "packages"
+        "shell"
+        "de"
+        "wm"
+        "terminal"
+        "cpu"
+        "gpu"
+        "memory"
+        "swap"
+        "display"
+        "bios"
+        "btrfs"
+        "localip"
+        "publicip"
+        "battery"
+        "bluetooth"
+        "sound"
+        "wifi"
+      ];
+    };
+  };
+
   programs.zsh = {
     enable = true;
     #enableCompletion = true;
