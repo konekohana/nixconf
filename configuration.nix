7ebc39321879e07936427f8f02e8a359d4cb56a3
Hana Volků <volek@adamv.cz>
2025-11-18 17:58:25 +0100

    install cargo rustc gcc
    This kind of setup doesn't feel ideal? Either I'd want rustup, or some
    sort of per project magic that will give me correct versions of the
    things I need. But This works for now I guess 😅

--- configuration.nix
+++ configuration.nix
@@ -173,6 +173,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    cargo
+    rustc
+    gcc
     vim
     wget
     vscode
