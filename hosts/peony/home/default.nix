9553db57e35ab0b3495d69a04228ff83bad2f871
Hana Volků <volek@adamv.cz>
2025-11-24 17:35:02 +0100

    cleanup: separate zsh configuration

--- hosts/peony/home/default.nix
+++ hosts/peony/home/default.nix
@@ -1,19 +1,11 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./zed.nix
+    ./zsh.nix
   ];
 
   home.username = "hana";
   home.homeDirectory = "/home/hana";
-
-  programs.pay-respects = {
-    enable = true;
-    enableZshIntegration = true;
-  };
 
   programs.fastfetch = {
     enable = true;
@@ -61,58 +53,6 @@
         "bluetooth"
         "sound"
         "wifi"
-      ];
-    };
-  };
-
-  programs.zsh = {
-    enable = true;
-    #enableCompletion = true;
-    #autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zsh.initContent
-    initContent = let
-      zshConfigEarlyInit = lib.mkOrder 500 ''
-        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-        # Initialization code that may require console input (password prompts, [y/n]
-        # confirmations, etc.) must go above this block; everything else may go below.
-        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-        fi
-      '';
-      zshConfig = lib.mkOrder 1000 "source ~/.p10k.zsh";
-    in
-      lib.mkMerge [zshConfigEarlyInit zshConfig];
-
-    # todo move the programs that I need for stuff in here to home manager config
-    shellAliases = {
-      #cargo = "cargo mommy";
-      gdc = "git diff --cached";
-      gsh = "git show --ext-diff";
-      cp = "cp --reflink=auto";
-      hddlist = ''printf "/dev/%s \n" $(lsblk -J -o name,rota | jq -r ".blockdevices[] | select(.rota).name")'';
-      hddstate = "sudo hdparm -C $(hddlist)";
-      hddstop = "sudo hdparm -y $(hddlist)";
-    };
-
-    history.size = 1000000000000;
-    history.ignoreAllDups = true;
-    history.path = "$HOME/.zsh_history";
-
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ];
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "virtualenv"
       ];
     };
   };
