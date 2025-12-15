9400ade2f7180d758c6ca7876e94b0453b8a0d8e
Hana Volků <volek@adamv.cz>
2025-12-15 20:21:11 +0100

    fwpower: make sure to always initialize the sign variable
    writeShellApplication adds `set -o nounset` to the script which makes it
    fail when the battery status is neither "Charging" nor "Discharging"
    (such as "Not charging" when the battery is charged up).

--- hosts/peony/home/scripts/fwpower.nix
+++ hosts/peony/home/scripts/fwpower.nix
@@ -9,21 +9,13 @@
 
         current="$(bc <<<"scale=5; $current_raw/1000000")"
         voltage="$(bc <<<"scale=5; $voltage_raw/1000000")"
-
         power="$(bc <<<"$voltage*$current")"
 
-        state="$(cat /sys/class/power_supply/BAT1/status)"
-
-        case "$state" in
-          Charging)
-            sign=+
-            ;;
-
-          Discharging)
-            sign=-
-            ;;
+        case "$(cat /sys/class/power_supply/BAT1/status)" in
+          Charging)    sign="+" ;;
+          Discharging) sign="-" ;;
+          *)           sign="" ;;
         esac
-
 
         LC_NUMERIC="en_US.UTF-8" printf "%s%.2f W\n" "$sign" "$power"
       '';
