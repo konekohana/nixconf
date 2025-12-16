a82aed87d6357fbf9b8e2318d0c9126fd3348fe6
Hana Volků <volek@adamv.cz>
2025-12-16 11:14:07 +0100

    fwpower: reduce cat abuse

--- hosts/peony/home/scripts/fwpower.nix
+++ hosts/peony/home/scripts/fwpower.nix
@@ -4,14 +4,14 @@
       name = "fwpower";
       runtimeInputs = [pkgs.bc];
       text = ''
-        current_raw="$(cat /sys/class/power_supply/BAT1/current_now)"
-        voltage_raw="$(cat /sys/class/power_supply/BAT1/voltage_now)"
+        current_raw="$(< /sys/class/power_supply/BAT1/current_now)"
+        voltage_raw="$(< /sys/class/power_supply/BAT1/voltage_now)"
 
         current="$(bc <<<"scale=5; $current_raw/1000000")"
         voltage="$(bc <<<"scale=5; $voltage_raw/1000000")"
         power="$(bc <<<"$voltage*$current")"
 
-        case "$(cat /sys/class/power_supply/BAT1/status)" in
+        case "$(< /sys/class/power_supply/BAT1/status)" in
           Charging)    sign="+" ;;
           Discharging) sign="-" ;;
           *)           sign="" ;;
