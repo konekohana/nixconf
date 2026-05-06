2ead680ce27580511fde3ef3f47c5558e286e9e4
Hana Volková <volek@adamv.cz>
2026-05-06 16:52:50 +0200

    Added NTK people count into train toolbar.
    I guess it's not much of a train toolbar anymore hehe.

--- hosts/peony/home/scripts/train-toolbar.nix
+++ hosts/peony/home/scripts/train-toolbar.nix
@@ -18,7 +18,9 @@
 
         function process_oebb()   { sed 's|.*|🚄🇦🇹  & km/h|'; }
         function process_cdwifi() { jq -r '"🚄  \(.speed) km/h  \(.altitude) m"'; }
+        function process_ntk()    { printf '📚 %s 👤' "$(grep -A5 'persons in the library' | grep -oP '<span>\s*\K[0-9]+')"; }
 
+        if getdata 'NTK-Staff' 'https://www.techlib.cz/en/' process_ntk; then exit 0; fi
         if getdata "CDWiFi" 'http://cdwifi.cz/portal/api/vehicle/realtime' process_cdwifi; then exit 0; fi
         if getdata "OEBB" 'https://railnet.oebb.at/api/speed' process_oebb; then exit 0; fi
       '';
