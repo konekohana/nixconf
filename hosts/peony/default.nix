5ac222ce97e0c80bdddee1f5f4be253f937540d5
Hana Volková <volek@adamv.cz>
2026-08-17 19:15:57 +0200

    peony: enable cups

--- hosts/peony/default.nix
+++ hosts/peony/default.nix
@@ -66,8 +66,12 @@
     '';
   };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   services.pipewire = {
     enable = true;
